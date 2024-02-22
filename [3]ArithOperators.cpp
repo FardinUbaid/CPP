d73c2a11cda923a04f7b12bd44a6efe55d1171b4
Fardin Abu Ubaid <103235862+FardinUbaid@users.noreply.github.com>
2024-02-22 21:01:23 +0600

    Add files via upload

---
+++ [3]ArithOperators.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+int main(){
+
+    // addition(+)/subtraction(-)/multiplication(*)/division(/)
+
+    int myNum = 1;
+    int myNum2 = 2;
+    int result = myNum + myNum2;
+
+    std::cout<<"The addition of num1 and num2 is ="<< result << '\n';
+
+    //modulous (%)
+
+    int myNum1 = 7;
+    int myNum3 = 2;
+    result = myNum1 % myNum3;
+
+    std::cout<<"The addition of num1 and num2 is ="<< result << '\n';
+
+}
