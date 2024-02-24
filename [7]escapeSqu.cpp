c0f36af311b977112da6249296698858c32e50b1
Fardin Abu Ubaid <103235862+FardinUbaid@users.noreply.github.com>
2024-02-24 20:45:00 +0600

    Add files via upload

---
+++ [7]escapeSqu.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+int main(){
+
+    /*
+        '\n' -> newline
+        '\t' -> tab -> 4 white spaces
+        '\a' -> produes a bip sound
+        '\f' -> form feed
+        '\b' -> add a back slash 
+        '\r' -> carraige return
+        '\\' -> adds a backslash
+         \'  -> adds a single coat
+        '\' -> 
+        '\nnn' -> octal number
+        '\xhh' -> hexadeciaml number
+        '\0' -> null
+        
+    */
+
+   std::cout<<"fardin\tfardin\tfardin\tfardin"<<'\n';
+   std::cout<<"fardin\tfardin\tfardin\tfardin"<<'\n';
+   std::cout<<"\a"<<'\n';
+
+
+
+}
